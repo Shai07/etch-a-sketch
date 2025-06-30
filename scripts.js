3b109887f1df1e5617e574e85264b9ab1f7d73ba
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-06-30 12:24:20 -0400

    added darkness value and moved modeHandler to grid

--- scripts.js
+++ scripts.js
@@ -20,26 +20,34 @@ function grid(size) {
         for (let j = 0; j < size; j++) {
             const tile = document.createElement('div');
             tile.className = 'tile';
+            tile.dataset.darkness = 0.0;
             row.appendChild(tile);
         }
+    }
+
+    const tiles = document.querySelectorAll('.tile')
+    for (let tile of tiles) {
+        tile.addEventListener('mouseover', () => modeHandler(tile));
     }
 }
 
 function paint(tile, color) {
     tile.style.backgroundColor = color;
-    return false;
+    tile.dataset.darkness = 0.0;
 }
 
 function paintRandom(tile) {
-    tile.style.backgroundColor = `rgb(${getRandomInt(255)},${getRandomInt(255)}, ${getRandomInt(255)}, 0)`;
+    tile.style.backgroundColor = `rgb(${getRandomInt(255)},${getRandomInt(255)}, ${getRandomInt(255)})`;
+    tile.dataset.darkness = 0.0;
 }
 
 function darken(tile) {
     let currentColor = getComputedStyle(tile).backgroundColor;
     let rgbaValues = currentColor.slice(currentColor.indexOf('(')+1, currentColor.indexOf(')')).split(', ');
     console.log(rgbaValues);
-    if (Number(rgbaValues[3]) < 1){
-        tile.style.backgroundColor = `rgba(${rgbaValues[0]}, ${rgbaValues[1]}, ${rgbaValues[2]}, ${Number(rgbaValues[3])+0.1})`;
+    if (tile.dataset.darkness < 1){
+        tile.dataset.darkness = parseFloat(tile.dataset.darkness) + 0.1;
+        tile.style.backgroundColor = `rgba(${rgbaValues[0]}, ${rgbaValues[1]}, ${rgbaValues[2]}, ${tile.dataset.darkness})`;
     }
 }
 
@@ -57,9 +65,6 @@ slider.addEventListener('change', function() {
 
 grid(size);
 
-const tiles = document.querySelectorAll('.tile')
-for (let tile of tiles) {
-    tile.addEventListener('mouseover', () => modeHandler(tile));
-}
+
 
 
